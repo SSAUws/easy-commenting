4ad46388d92e101d3a48756a80ef084cef27c30d
nemomojie <nemomojie@gmail.com>
2013-04-08 14:56:26 +0800

    add icon files

---
+++ assets/www/index.js
@@ -0,0 +1,20 @@
+function onLoad()
+{
+	document.addEventListener("deviceready",onDeviceReady,false);
+}
+
+function onDeviceReady()
+{
+	document.addEventListener("offline",offlineAlert,false);
+	document.addEventListener("online",onlineAlert,false);
+}
+
+function offlineAlert()
+{
+	alert(0);
+}
+
+function onlineAlert()
+{
+	alert(1);
+}
