2b66c7b6b2d385406c466e80376038c8d3db957a
nemomojie <nemomojie@gmail.com>
2013-04-09 22:02:17 +0800

    add barcode scanner

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -41,3 +41,16 @@ function weibo()
 {
 	// get access token
 }
+
+function barcodeScan()
+{
+	window.plugins.barcodeScanner.scan( function(result) {
+		alert("We got a barcode\n" +
+			"Result: " + result.text + "\n" +
+			"Format: " + result.format + "\n" +
+			"Cancelled: " + result.cancelled);
+	}, function(error) {
+		alert("Scanning failed: " + error);
+	}
+	);
+}
