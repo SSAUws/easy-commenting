5b3fe8aa99823f6f74cf45cf18e2864d584870cc
nemomojie <nemomojie@gmail.com>
2013-04-22 21:17:16 +0800

    @ & emotion

---
+++ assets/www/js/emotion.js
@@ -0,0 +1,100 @@
+var emotion;
+
+function emotionInit()
+{
+	emotion = new Array;
+	emotion.push("");
+	emotion.push("蜡烛");
+	emotion.push("许愿");
+	emotion.push("泪流满面");
+	emotion.push("转发");
+	emotion.push("江南style");
+	emotion.push("偷乐");
+	emotion.push("可爱");
+	emotion.push("gst耐你");
+	emotion.push("lt切克闹");
+	emotion.push("moc转发");
+	emotion.push("草泥马");
+	emotion.push("神马");
+	emotion.push("浮云");
+	emotion.push("给力");
+	emotion.push("围观");
+	emotion.push("威武");
+	emotion.push("熊猫");
+	emotion.push("兔子");
+	emotion.push("奥特曼");
+	emotion.push("囧");
+	emotion.push("互粉");
+	emotion.push("礼物");
+	emotion.push("呵呵");
+	emotion.push("嘻嘻");
+	emotion.push("哈哈");
+	emotion.push("可爱");
+	emotion.push("可怜");
+	emotion.push("挖鼻屎");
+	emotion.push("吃惊");
+	emotion.push("害羞");
+	emotion.push("挤眼");
+	emotion.push("闭嘴");
+	emotion.push("鄙视");
+	emotion.push("爱你");
+	emotion.push("泪");
+	emotion.push("偷笑");
+	emotion.push("亲亲");
+	emotion.push("生病");
+	emotion.push("太开心");
+	emotion.push("懒得理你");
+	emotion.push("右哼哼");
+	emotion.push("左哼哼");
+	emotion.push("嘘");
+	emotion.push("衰");
+	emotion.push("委屈");
+	emotion.push("吐");
+	emotion.push("打哈欠");
+	emotion.push("抱抱");
+	emotion.push("怒");
+	emotion.push("疑问");
+	emotion.push("馋嘴");
+	emotion.push("拜拜");
+	emotion.push("思考");
+	emotion.push("汗");
+	emotion.push("困");
+	emotion.push("睡觉");
+	emotion.push("钱");
+	emotion.push("失望");
+	emotion.push("酷");
+	emotion.push("花心");
+	emotion.push("哼");
+	emotion.push("鼓掌");
+	emotion.push("晕");
+	emotion.push("悲伤");
+	emotion.push("抓狂");
+	emotion.push("黑线");
+	emotion.push("阴险");
+	emotion.push("怒骂");
+	emotion.push("心");
+	emotion.push("伤心");
+	emotion.push("猪头");
+	emotion.push("ok");
+	emotion.push("耶");
+	emotion.push("good");
+	emotion.push("不要");
+	emotion.push("赞");
+	emotion.push("来");
+	emotion.push("弱");
+	emotion.push("蛋糕");
+	emotionContainerInit();
+}
+
+function emotionContainerInit()
+{
+	var i;
+	var obj = document.getElementById("emotion-container");
+	var inner = "";
+	for (i = 1 ; i < emotion.length ; i++)
+	{
+		var s = "[" + emotion[i] + "]";
+		inner += '<img src="emotion/' + i + '.gif"' + ' alt="' + emotion[i] + '" onclick="addStrToContent(' + "'" + s + "'" + ')"/>\n';
+	}
+	obj.innerHTML = inner;
+}
--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -2,7 +2,16 @@ var access_token;
 var uid;
 var user_name;
 var user_img;
-var last_page = "home";
+var last_page = "atUser";
+var last_at;
+
+function changeAfterLogin()
+{
+	changeLoginState("home-login");
+	changeLoginState("view-login");
+	changeLoginState("history-login");
+	changeLoginWidth();
+}
 
 function testMode()
 {
@@ -13,22 +22,33 @@ function testMode()
 
 function checkLocalStorage()
 {
-	if (localStorage.length == 2)
+	//	localStorage.clear();
+	if (localStorage.access_token != undefined)
 	{
 		access_token = localStorage.access_token;
 		uid = localStorage.uid;
 		getUserInfo();
 	}
+	if (localStorage.last_at != undefined)
+	{
+		var s = localStorage.last_at;
+		last_at = s.split(",");
+	}
+	else last_at = new Array;
 }
-
-function onLoad()
-{
-	access_token = null;
-	user_name = null;
-	networkNoteHideup();
-	checkLocalStorage();
-	document.addEventListener("deviceready",onDeviceReady,false);
-}
+(function($){
+	$(window).load(function()
+		{
+			emotionInit();
+			access_token = null;
+			user_name = null;
+			networkNoteHideup();
+			checkLocalStorage();
+			testMode();
+			document.addEventListener("deviceready",onDeviceReady,false);
+			$(".sth-content").mCustomScrollbar();
+		});
+})(jQuery);
 
 function onDeviceReady()
 {
@@ -67,9 +87,7 @@ function exitApp()
 	navigator.app.exitApp();
 }
 
-
 //weibo event handle
-
 function weibo()
 {
 	var redirect_uri = "https://api.weibo.com/oauth2/default.html";
@@ -89,10 +107,8 @@ function weibo()
 			localStorage.uid = uid;
 			plugins.childBrowser.close();
 			getUserInfo(); 
+			jumpto(last_page);
 		} 
-		else {
-			console.log("other");
-		}
 	};
 }
 
@@ -106,11 +122,7 @@ function getUserInfo()
 			var json = json_parse(xmlhttp.responseText);
 			user_name = json.screen_name;
 			user_img = json.profile_image_url;			
-			changeLoginState("home-login");
-			changeLoginState("view-login");
-			changeLoginState("history-login");
-			changeLoginWidth();
-			jumpto(last_page);
+			changeAfterLogin();
 		}
 	}
 	xmlhttp.open("GET", url, true);
@@ -119,33 +131,26 @@ function getUserInfo()
 
 function changeLoginWidth()
 {
-	$(".my-breakpoint-1.ui-grid-c .ui-block-a").css("width","10%");
-	$(".my-breakpoint-1.ui-grid-c .ui-block-a").css("margin","0");
-	$(".my-breakpoint-1.ui-grid-c .ui-block-b").css("width","20%");
-	$(".my-breakpoint-1.ui-grid-c .ui-block-b").css("margin-left","2.5%");
-	$(".my-breakpoint-1.ui-grid-c .ui-block-b").css("margin-right","2.5%");
+	$(".my-breakpoint-1.ui-grid-b .ui-block-a").css("width","35%");
+	$(".my-breakpoint-1.ui-grid-b .ui-block-a").css("margin","0");
 }
 
 function changeLoginState(id)
 {
-	document.getElementById(id+"-1").innerHTML='<img src="'+user_img+'" alt="" width="expression(this.width > 50 ? "50px": "100%")"/>';
-	document.getElementById(id+"-2").innerHTML='<p style="text-overflow:ellipsis; white-space:nowrap;overflow:hidden;">'+user_name+'</p>';
+	document.getElementById(id).innerHTML='<p style="text-overflow:ellipsis; white-space:nowrap;overflow:hidden;">'+user_name+'</p>';
 }
 
 function login()
 {
-	if (user_name == null) jumpto("login");
-	else jumpto("sendWeibo");
+	//	if (user_name == null) jumpto("login");
+	//	else jumpto("sendWeibo");
+	jumpto("sendWeibo");
 }
 
 jQuery( window ).on( "hashchange",function()
 		{
 			var hash = location.hash;
-			if (hash != "#login")
-			{
-				last_page = hash;
-				last_page = last_page.substr(1);
-			}
+			if (hash != "#login") last_page = hash.substr(1);
 		})
 
 function send_weibo()
@@ -153,12 +158,12 @@ function send_weibo()
 	var s = document.getElementById("weibo-content").value;
 	var encode_s = encodeURIComponent(s);
 	var url = "https://api.weibo.com/2/statuses/update.json?status=" + encode_s + "&access_token=" + access_token;
-	alert(url);
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) 
 		{
 			alert("Send Successfully!");
+			document.getElementById("weibo-content").value = "";
 		}
 	}
 	xmlhttp.open("POST", url, true);
@@ -166,26 +171,132 @@ function send_weibo()
 	xmlhttp.send();
 }
 
+var friends;
+
+function addStrToContent(str)
+{
+	var obj = document.getElementById("weibo-content");
+	var i;
+	if (str[0] == '@')
+	{
+		for (i = 0 ; i < last_at.length ; i++)
+			if (last_at[i] == str) last_at.splice(i,1);
+		last_at.push(str);
+		while (last_at.length > 3)	last_at.shift();
+		localStorage.last_at = last_at.toString();
+	}
+	if (document.selection) 
+	{
+		var sel = document.selection.createRange();
+		sel.text = str;
+	} 
+	else 
+		if (typeof obj.selectionStart === 'number' && typeof obj.selectionEnd === 'number') 
+		{
+			var startPos = obj.selectionStart,
+				endPos = obj.selectionEnd,
+				cursorPos = startPos,
+				tmpStr = obj.value;
+			obj.value = tmpStr.substring(0, startPos) + str + tmpStr.substring(endPos, tmpStr.length);
+			cursorPos += str.length;
+			obj.selectionStart = obj.selectionEnd = cursorPos;
+		}
+		else 
+		{
+			obj.value += str;
+		}
+	jumpto("sendWeibo");
+}	
+
+function updateList(flag)
+{
+	var ul = document.getElementById("user-list");
+	var inner = "";
+	var i;
+	if (flag)
+	{
+		inner += "<div>Lastest @</div>";
+		for (i = last_at.length - 1; i >= 0 ; i--)
+			inner += '<div onclick="addStrToContent('+"'"+"@"+last_at[i]+" "+"'"+')">'+last_at[i]+"</div>";
+	}
+	inner += "<hr/><div>Your friends</div>"
+		for (i = 0 ; i < friends.length ; i++)
+			inner += '<div onclick="addStrToContent('+"'"+"@"+friends[i]+" "+"'"+')">'+friends[i]+"</div>";
+	ul.innerHTML = inner;
+}
+
+function updateFriends1()
+{
+	var url = "https://api.weibo.com/2/friendships/friends.json?uid="+uid+"&count=10&access_token="+access_token;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
+	xmlhttp.onreadystatechange = function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) 
+		{
+			var json = json_parse(xmlhttp.responseText);
+			delete friends;
+			friends = new Array(json.users.length);;
+			var i;
+			for (i = 0 ; i < json.users.length ; i++)
+				friends[i] = json.users[i].screen_name;
+			updateList(true);
+		}
+	}
+}
+
+function updateFriends2()
+{
+	var s = document.getElementById("atUser-input").value;
+	s = encodeURIComponent(s);
+	var url="https://api.weibo.com/2/search/suggestions/at_users.json?type=0&count=10&q="+s+"&access_token="+access_token;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET",url,true);
+	xmlhttp.send();
+	xmlhttp.onreadystatechange = function(){
+		if (xmlhttp.readyState==4 && xmlhttp.status==200) 
+		{
+			var json = json_parse(xmlhttp.responseText);
+			delete friends;
+			friends = new Array(json.length+1);
+			friends[0] = document.getElementById("atUser-input").value
+				var i;
+			for (i = 0 ; i < json.length ; i++)
+				friends[i+1] = json[i].nickname;
+			updateList(false);
+		}
+	}
+}
+
+function atUserInit()
+{
+	document.getElementById("atUser-input").value = "";
+	updateFriends1();
+}
+
+function atUser()
+{
+	var s = document.getElementById("atUser-input").value;
+	if (s == "") updateFriends1();
+	else updateFriends2();
+}
+
+function setFocus(s)
+{
+	document.getElementById(s).focus();
+}
+
 //barcode scan
 function barcodeScan()
 {
 	window.plugins.barcodeScanner.scan( function(result) {
-		document.getElementById("barcode-decode").innerHTML =
-		("We got a barcode<br/>" +
-		 "Result: " + result.text + "<br/>" +
-		 "Format: " + result.format + "<br/>" +
-		 "Cancelled: " + result.cancelled);
-	jumpto("view");
+		alert("We got a barcode<br/>" +
+			"Result: " + result.text + "<br/>" +
+			"Format: " + result.format + "<br/>" +
+			"Cancelled: " + result.cancelled);
+		jumpto("view");
 	}, function(error) {
 		alert("Scanning failed: " + error);
 	}
 	);
-}
-
-//select menu handle
-function select(sobj,s)
-{
-	var goal = sobj.options[sobj.selectedIndex].value;
-	document.getElementById(s+'-select').value = s;
-	if (goal != s) jumpto(goal);
 }
