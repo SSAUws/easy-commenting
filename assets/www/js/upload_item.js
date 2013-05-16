cadc2903dacd92a4efc65f6d4b3e660cfde5d8c9
nemomojie <nemomojie@gmail.com>
2013-05-16 12:09:07 +0800

    aaa

--- assets/www/js/upload_item.js
+++ assets/www/js/upload_item.js
@@ -1,30 +1,33 @@
-function checkContentNotEmpty() {
+function checkContentNotEmpty() 
+{
 	return ($("#upload_item_name").val() != "" && 
 			$("#upload_item_intro").val() != "" );
 }
 
-function uploadBarcodeInfo() {
-	if (!checkContentNotEmpty()) {
+function uploadBarcodeInfo() 
+{
+	if (!checkContentNotEmpty()) 
+	{
 		$("#popupBecauseEmpty").popup("open");
 		return ;
 	}
-	
+	$.mobile.loading('show',{text:"上传中",textVisible:true});
 	var oOutput = document.getElementById("output"), oData = new FormData(
 			document.forms.namedItem("barcodeinfo"));
 	oData.append("id", itemId);
+	console.log(oData);
 
 	var oReq = new XMLHttpRequest();
 	oReq.open("POST", host + '/barcodeupload', true);
 	oReq.onload = function(oEvent) {
 		if (oReq.status == 200) {
-			oOutput.innerHTML = "Uploaded!";
-			jumpto("view");
-			viewRefresh();
+			oOutput.innerHTML = "上传成功，即将跳转";
+			$.mobile.loading('hide');
+			setTimeout('jumpto("view");'+'viewRefresh();',2000);
 		} else {
-			oOutput.innerHTML = "Error " + oReq.status
-					+ " occurred uploading your file.<br \/>";
+			oOutput.innerHTML = "错误 " + oReq.status
+					+ " 上传物品失败<br/>";
 		}
 	};
-
 	oReq.send(oData);
 }
